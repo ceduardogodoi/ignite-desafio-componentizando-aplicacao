fd6b7f655833a85baee0ee86d9a4d61ffcc12377
Carlos Eduardo Alves de Godoi <carlosg@jmalucelliti.com.br>
2021-03-22 21:34:39 -0300

    ignite: finaliza desafio 2

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+import { MovieProps } from '../models/MovieProps';
+
+type ContentProps = {
+  movies: MovieProps[];
+  genre: string;
+};
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {props.genre}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { Button } from './Button';
+
+import { GenreResponseProps } from '../models/GenreResponseProps';
+
+import { api } from '../services/api';
+
+type SideBarProps = {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleGenreClick(id: number): void;
+};
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleGenreClick(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
